f990bf7cd9ca7c7324a51ded5079fdbe37b16071
sagul-09 <nihal.evil.royal@gmail.com>
2023-08-19 20:35:06 +0530

    register Gform update

--- src/components/register/register.jsx
+++ src/components/register/register.jsx
@@ -7,7 +7,11 @@ function register() {
           REGISTER NOW TO BE A PART OF THE TECH-SOCIETY COMMUNITY
         </div>
         <div className="wrapper">
-          <a className="cta" href="#">
+          <a
+            className="cta"
+            target="blank"
+            href="https://form.everestwebdeals.co/?form=1FAIpQLSf18TDbuU-id3qhAiphlrBYGnIxrY2UGdLZ3QVl7JFU5YwGNQ"
+          >
             <span>REGISTER</span>
           </a>
         </div>
