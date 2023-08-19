f67af8d1ab6bcbeae7ef818c8121153945a6bb7f
sagul-09 <nihal.evil.royal@gmail.com>
2023-08-19 15:26:19 +0530

    app.jsx update

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,13 @@
-// import React, { useCallback } from "react";
 import Nav from "./components/nav/nav";
 import Home from "./components/home/home";
 import About from "./components/about/about";
-import Community from "./components/community/community"
+import Community from "./components/community/community";
 import Register from "./components/register/register";
 import Footer from "./components/footer/footer";
-// import Particles from "react-tsparticles";
-// import { Particle } from "./configs/partical.config";
-import { loadFull } from "tsparticles";
 
 const App = () => {
-  // const particlesInit = useCallback(async (engine) => {
-  //   await loadFull(engine);
-  // }, []);
-
-  // const particlesLoaded = useCallback(async (container) => {}, []);
   return (
     <>
-      {/* <Particles
-        className="particles"
-        id="tsparticles"
-        init={particlesInit}
-        loaded={particlesLoaded}
-        options={Particle}
-      /> */}
       <Nav />
       <Home />
       <About />
