0cb525f4eb3365a93d1aa3c642faa036f2862ec5
sagul-09 <nihal.evil.royal@gmail.com>
2023-08-19 08:51:38 +0530

    fixed teh about vision mission card

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -6,15 +6,15 @@ const about = () => {
       <div className="About-Title">ABOUT</div>
       <div className="About-Society">
         Welcome to our dynamic and innovative Tech Society! Your gateway to the
-        exciting world of new technologies. A community created by the learners,
-        of the learners, and for the learners. Through engaging workshops,
+        exciting world of new technologies.<br/> A community created by the learners,
+        of the learners, and for the learners.<br/> Through engaging workshops,
         thrilling hackathons, and collaborative project support, we're here to
         ignite your passion for technology and empower you to shape the future.
         Join us on this journey of exploration, creation, and endless
         possibilities!
       </div>
       <div className="Vision-Mission-Container">
-        <div className="About-Vision">
+    
           {/* <div className="Vision-Title">VISION</div> */}
           <div className="Vision-Content">
             <div className="card">
@@ -33,9 +33,9 @@ const about = () => {
                 </li>
               </ul>
             </div>
-          </div>
+          
         </div>
-        <div className="About-Mission">
+      
           {/* <div className="Mission-Title">Mission</div> */}
           <div className="Mission-Content">
             <div className="card">
@@ -54,7 +54,7 @@ const about = () => {
             </div>
           </div>
         </div>
-      </div>
+    
     </div>
   );
 };
